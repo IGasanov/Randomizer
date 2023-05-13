25198b40c6e16b4af07b1d28c8fee4ffe9d4885a
Gamzat <gamzik44@gmail.com>
2023-05-14 00:05:56 +0300

    final commit

---
+++ Randomizer/MainViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Randomizer
+//
+//  Created by Гамзат Гасанов on 13.05.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    @IBOutlet var randomValueLabel: UILabel!
+    @IBOutlet var getRandomNumberButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getRandomNumberButton.layer.cornerRadius = 10
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.minimumValue = minimumValueLabel.text
+        settingsVC.maximumValue = maximumValueLabel.text
+    }
+    
+    @IBAction func getRandomNumberTapped() {
+    let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
+    let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 100
+    
+    randomValueLabel.text = Int.random(in:
+        minimumNumber...maximumNumber).formatted()
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let settingVC = segue.source as? SettingsViewController else { return }
+        minimumValueLabel.text = settingVC.minimumValueTF.text
+        maximumValueLabel.text = settingVC.maximumValueTF.text
+    }
+}
+
